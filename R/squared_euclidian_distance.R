c6eb149ba659de8b3c758aafa6234452b48e86ca
nevrome <clemens@nevrome.de>
2018-04-30 14:51:36 +0200

    outsourcing sed calculation for simulation

--- R/squared_euclidian_distance.R
+++ R/squared_euclidian_distance.R
@@ -3,3 +3,26 @@ sed <- function(pi, pj) {
   pj <- pj / sum(pj)
   sum((pi - pj)^2)
 }
+
+calculate_sed_for_group_drift_simulation_result <- function(pop_devel_sum, sim_run = 1) {
+
+  groups <- pop_devel_sum %>%
+    base::split(., .$group)
+  
+  A <- groups$A %>%
+    base::split(., .$time)
+  B <- groups$B %>%
+    base::split(., .$time)
+  
+  A <- lapply(A, function(x) {x$individuals_with_variant})
+  B <- lapply(B, function(x) {x$individuals_with_variant})
+  
+  sed_res <- purrr::map2_dbl(A, B, function(a, b) {sed(a, b)})
+  
+  tibble::tibble(
+    t = unique(pop_devel_sum$time),
+    sed = sed_res,
+    sim_run = sim_run
+  )
+  
+}
