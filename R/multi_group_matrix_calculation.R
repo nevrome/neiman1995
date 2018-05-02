31c10424f48dd0730b859e01fb5ee6366804f146
nevrome <clemens@nevrome.de>
2018-05-02 13:13:04 +0200

    started to make the matrix calculation more flexible

--- R/multi_group_matrix_calculation.R
+++ R/multi_group_matrix_calculation.R
@@ -1,27 +1,19 @@
-multi_group_matrizes <- function(Ne, t) {
+multi_group_matrizes <- function(Ne, t, Mk) {
   
-  amount_groups <- 5
+  k <- 1
+  amount_groups <- nrow(Mk(k))
 
   group_matrix <- matrix(data = rep(0, amount_groups^2), nrow = amount_groups, ncol = amount_groups)
-  
+
+  M <- Mk
+    
   U <- group_matrix
   diag(U) <- 1/Ne
   
-  M <- function(k) {
-    M <- group_matrix
-    #M[] <- 0.2
-    M[1,] <- c(0.4, 0.0, 0.2, 0.0, 0.4)
-    M[2,] <- c(0.0, 0.4, 0.2, 0.4, 0.0)
-    M[3,] <- c(0.2, 0.2, 0.2, 0.2, 0.2)
-    M[4,] <- c(0.2, 0.1, 0.3, 0.2, 0.2)
-    M[5,] <- c(0.2, 0.3, 0.1, 0.2, 0.2)
-    M
-  }
-  
   matrix_time_list <- list()
   
-  for (k in 1:t) {
-    matrix_time_list[[k]] <- M(k)  %*% U  %*% t(M(k)) 
+  for (pk in 1:t) {
+    matrix_time_list[[pk]] <- M(k)  %*% U  %*% t(M(k)) 
   }
   
   V <- Reduce(`+`, matrix_time_list) 
